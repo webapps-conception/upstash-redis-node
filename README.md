45d61674fbe1ffe15f8f4d182d0493ab0d72bdc5
Rudy <rudy@paloma.tr>
2023-05-26 21:33:02 +0200

    Initialisation du projet upstash-redis-node.

---
+++ README.md
@@ -0,0 +1,8 @@
+# upstash-redis-node
+* https://github.com/upstash/upstash-redis/tree/main
+
+## Configuration de l'authentification redis dans le fichier .env :
+```
+REDIS_URL = ''
+REDIS_TOKEN = ''
+```
---
+++ redis.mjs
@@ -0,0 +1,37 @@
+import { Redis } from '@upstash/redis'
+
+/* Chargement autiomatique du fichier .env */
+import * as dotenv from 'dotenv' 
+dotenv.config()
+
+const redis = new Redis({
+  url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
+})
+
+// string
+await redis.set('key', 'value');
+let data = await redis.get('key');
+console.log(data)
+
+await redis.set('key2', 'value2', {ex: 1});
+
+// sorted set
+await redis.zadd('scores', { score: 1, member: 'team1' })
+data = await redis.zrange('scores', 0, 100 )
+console.log(data)
+
+// list
+await redis.lpush('elements', 'magnesium')
+data = await redis.lrange('elements', 0, 100 )
+console.log(data)
+
+// hash
+await redis.hset('people', {name: 'joe'})
+data = await redis.hget('people', 'name' )
+console.log(data)
+
+// sets
+await redis.sadd('animals', 'cat')
+data  = await redis.spop('animals', 1)
+console.log(data)
